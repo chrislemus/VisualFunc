7c16b4610b04cde9c6faa8f2f524610a235bd4c5
chrislemus <icrislemus@gmail.com>
2021-06-24 16:33:24 -0400

    add css normalize, material ui, and font dependancy

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/normalize.css';
+import '../styles/index.css';
+import '@fontsource/roboto';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
